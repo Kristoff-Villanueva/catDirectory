449a53897b4b2a10d64d187c6186a4bed1f70e59
Kristoff-Villanueva <kristoffer.villanueva18@gmail.com>
2022-10-22 23:05:31 +0800

    start fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import Card from "./assets/components/Card";
 import Footer from "./assets/components/Footer";
 
 function App() {
+	async function getResponse() {
+		const response = await fetch("https://api.thecatapi.com/v1/images/search");
+		const data = await response.json();
+		console.log(data);
+	}
+
+	getResponse();
+
 	return (
 		<div className="App">
 			<Card />
--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import cardModule from "./card.module.css";
 
-export default function Card() {
+export default function Card(props) {
 	return (
 		<div className="card">
-			<h1 className={cardModule.h1}>This is the card component</h1>
+			<img src="" alt="" />
 		</div>
 	);
 }
